146bf2957bc88856ad19474e3ae66d8c106cb162
VitorGoatman <vitor.goatman@gmail.com>
2020-12-02 22:32:31 -0300

    Add modal de seleção de status

--- js/lista.js
+++ js/lista.js
@@ -16,8 +16,10 @@ function makeItem(data){
 	data.forEach(doc => {
 		var info = doc.data();
 		var date = info.dia+"/"+info.mes+"/"+info.ano;
-		if (info.dia == dia1 && info.mes == mes1) {
-			item +="<div onclick=\"getInfo()\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
+		var d = dia1-info.dia;
+		var m = mes1-info.mes;
+		if (hoje == date) {
+			item +="<div id='"+doc.id+"' onclick=\"getInfo("+doc.id+")\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
 			item +="<strong id='orçamento' class='w3-large'>"+ doc.id +"</strong>";
 			item +="<strong class='w3-large w3-right'>"+ info.hora+":"+info.min+"</strong>";
 			item +="<div class='flex'>";
@@ -27,8 +29,8 @@ function makeItem(data){
 			item +="<div class='w3-center w3-text-green'><strong>Confirmado</strong></div>";
 			item +="</div>";
 		}
-		else if (info.dia < dia1 && info.mes == mes1) {
-			itemOntem +="<div onclick=\"getInfo()\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
+		else if (d == 1 && m==0) {
+			itemOntem +="<div onclick=\"getInfo("+doc.id+")\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
 			itemOntem +="<strong id='orçamento' class='w3-large'>"+ doc.id +"</strong>";
 			itemOntem +="<div class='flex'>";
 			itemOntem +="<div class='rect'>"+ info.empresa +"</div>";
@@ -37,8 +39,8 @@ function makeItem(data){
 			itemOntem +="<div class='w3-center w3-text-green'><strong>Confirmado</strong></div>";
 			itemOntem +="</div>";
 		}
-		else if (info.dia > dia1 && info.mes < mes1) {
-			itemOntem +="<div onclick=\"getInfo()\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
+		else if (d < 0 && m == 1 ) {
+			itemOntem +="<div onclick=\"getInfo("+doc.id+")\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
 			itemOntem +="<strong id='orçamento'  class='w3-large'>"+ doc.id +"</strong>";
 			itemOntem +="<div class='flex'>";
 			itemOntem +="<div class='rect'>"+ info.empresa +"</div>";
@@ -48,7 +50,7 @@ function makeItem(data){
 			itemOntem +="</div>";
 		}
 		else {
-			itemWeek +="<div onclick=\"getInfo()\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
+			itemWeek +="<div onclick=\"getInfo("+doc.id+")\" class='w3-padding-small w3-hover-white w3-border-blue-gray w3-border-top w3-border-bottom'>";
 			itemWeek +="<strong id='orçamento' class='w3-large'>"+ doc.id +"</strong>";
 			itemWeek +="<strong class='w3-large w3-right'>"+ date +"</strong>";
 			itemWeek +="<div class='flex'>";
@@ -64,7 +66,26 @@ function makeItem(data){
 		document.getElementById('hoje').innerHTML = item;
 }
 
-function getInfo() {
+function getInfo(id) {
+	document.getElementById('registro').innerHTML = "Atualizar Registro: "+id;
 	document.getElementById('info').style.display='block';
-	document.getElementById('dados').innerHTML = "Isso é um teste";
+	var orca = id;
+	var docRef = db.collection("Orçamentos").doc("'"+orca+"'");
+
+	docRef.get().then((snapshot)=>{
+			var docu = snapshot.data();
+		console.log(snapshot.id);
+			document.getElementById('obs').innerHTML = docu.nome;
+	}).catch(function(error) {
+		console.log("Error getting document:", error);
+	});
+}
+
+function mudarStatus(id) {
+	status = document.getElementById('setStatus').valur;
+	switch (status) {
+		case 0:
+			document.getElementById(id).style.backgroundColor= 'red';
+			break;
+	}
 }
